b2aab2f67ea9df36978d87f7ba5a93e2239ca66c
vpdacoder@gmail.com <vaipatel@utexas.edu>
2018-06-26 16:56:46 -0500

    API responding to the user input

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.set("view engine","ejs");
 
+app.get("/", (req,res)=>{
+  res.render('search');
+});
+
+// app.post("/", (req,res)=>{
+//   // userInput = req.body.name;
+//   // console.log(userInput);
+//   res.redirect('/results');
+// });
+
 app.get("/results", (req,res) => {
-  request('http://www.omdbapi.com/?apikey=thewdb&s=bond', function (error, response, body) {
+  query = req.query.name;
+  url = 'http://www.omdbapi.com/?apikey=thewdb&s='+ query;
+  request(url, function (error, response, body) {
   var parsedData = JSON.parse(body);
+  // console.log(parsedData);
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  res.send(parsedData); // Print the HTML for the Google homepage.
+  res.render("display",{data:parsedData}); // Print the HTML for the Google homepage.
 });
 });
 
